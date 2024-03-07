c0ef978916275f932eef3a22f8d13889b6b81f29
Wesley Austin <131537918+worldwidewes33@users.noreply.github.com>
2024-03-07 15:32:01 -0700

    moved state up to board component

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,43 @@
 import { useState } from "react";
+import PropTypes from "prop-types";
 
 let user = "X";
 
-function Square() {
-  const [value, setValue] = useState("");
-
-  const handleClick = () => {
-    setValue(user);
-    user = user === "X" ? "O" : "X";
-  };
-
+function Square({ value, onSquareClick }) {
   return (
-    <button className="square" onClick={handleClick}>
+    <button className="square" onClick={onSquareClick}>
       {value}
     </button>
   );
 }
 
+Square.propTypes = {
+  value: PropTypes.string.isRequired,
+  onSquareClick: PropTypes.func,
+};
+
 function Board() {
+  const [squares, setSquares] = useState(new Array(9).fill(null));
+
+  function handleClick(index) {
+    const newSquares = [...squares];
+
+    newSquares[index] = user;
+    user = user === "X" ? "O" : "X";
+    setSquares(newSquares);
+  }
+
   return (
     <div className="container">
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
+      <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+      <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+      <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+      <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+      <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+      <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+      <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+      <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+      <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
     </div>
   );
 }
